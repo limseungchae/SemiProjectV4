8802e41d00d011c98e01ab79cf0112c359f004da
limseungchae <tmdco12@naver.com>
2023-05-01 11:49:27 +0900

    bootstrap5 라이브러리 및 타일즈3 적용 확인

---
+++ src/main/java/seung/spring4/mvc/semeprojectv4/controller/indexController.java
@@ -0,0 +1,13 @@
+package seung.spring4.mvc.semeprojectv4.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class indexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index.tiles";
+    }
+}
