e36e925ce6cbc8439292675c996c36caa4eb11a4
limseungchae <tmdco12@naver.com>
2023-05-02 17:16:46 +0900

    회원가입 - 아이디 중복체크 RESTful 1 - controller

--- src/main/java/seung/spring4/mvc/semeprojectv4/controller/joinController.java
+++ src/main/java/seung/spring4/mvc/semeprojectv4/controller/joinController.java
@@ -51,12 +51,26 @@ public class joinController {
         try {
             // 응답유형은 JSON으로 설정
             res.setContentType("application/json; charset=utf-8");
-            // 검색된 ㅇ편번호 결과를 뷰없이 바로 응답으로 출력
+            // 검색된 우편번호 결과를 뷰없이 바로 응답으로 출력
             res.getWriter().print( jnsrv.findZipcode(dong) );
         } catch (IOException ex) {
             ex.printStackTrace();
         }
 
+    }
 
+    // 아이디 사용가능여부 검사
+    // /join/checkuid?uid=아이디
+    // 사용가능 : 0
+    // 사용불가능 : 1
+    @ResponseBody
+    @GetMapping("/checkuid")
+    public void checkuid(String uid, HttpServletResponse res) {
+        try {
+            // 아이디 사용여부를 뷰없이 바로 응답으로 출력
+            res.getWriter().print( jnsrv.checkUserid(uid) );
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
