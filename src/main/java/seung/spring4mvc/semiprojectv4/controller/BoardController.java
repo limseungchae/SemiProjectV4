a1b2572105a0e12b4e11d7d03a4cd91a701ec7d5
limseungchae <tmdco12@naver.com>
2023-05-04 17:15:45 +0900

    게시판 list 5 : 페이지네이션 3 - 페이지네이션 블럭 처리

--- src/main/java/seung/spring4mvc/semiprojectv4/controller/BoardController.java
+++ src/main/java/seung/spring4mvc/semiprojectv4/controller/BoardController.java
@@ -5,13 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import seung.spring4mvc.semiprojectv4.service.BordeService;
+import seung.spring4mvc.semiprojectv4.service.BoardService;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
-    @Autowired private BordeService bdsrv;
+    @Autowired private BoardService bdsrv;
 
 
     @GetMapping("/list")
@@ -20,8 +20,11 @@ public class BoardController {
         // System.out.println(bdsrv.readBoard(cpg).size());
 
         mv.setViewName("board/list.tiles");
+
         mv.addObject("bdlist",bdsrv.readBoard(cpg));
         mv.addObject("cpg", cpg);
+        mv.addObject("stpg", ((cpg - 1) / 10) * 10 + 1);
+
 
         return mv;
     }
