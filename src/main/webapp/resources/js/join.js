3654d2ba43468e231357acaba072809d99353e82
limseungchae <tmdco12@naver.com>
2023-05-19 11:34:12 +0900

    모달 오류 수정

--- src/main/webapp/resources/js/join.js
+++ src/main/webapp/resources/js/join.js
@@ -1,9 +1,3 @@
-// header 로고 클릭 이벤트
-let logo = document.querySelector('#logo');
-logo.addEventListener('click', () => {
-    location.href = '/';
-});
-
 // ------------------------------- agree
 // 이용약관 체크박스
 let agree1 = document.querySelector('#agree1');
@@ -28,7 +22,6 @@ noagree?.addEventListener('click', () => {
 // ------------------------------- checkme
 const chkfrm2 = document.forms.checkfrm2;
 const chk2btn = document.querySelector('#check2btn');
-
 chk2btn?.addEventListener('click', () => {
     if (chkfrm2.name.value === '') alert('이름을 입력하세요!!');
     else if (chkfrm2.jumin1.value === '') alert('주민번호를 입력하세요!!');
@@ -40,7 +33,6 @@ chk2btn?.addEventListener('click', () => {
         chkfrm2.submit();
     }
 });
-
 
 // ------------------------------- joinme
 const joinfrm = document.forms.joinfrm;
@@ -58,21 +50,18 @@ const pwdmsg = document.querySelector('#pwdmsg');
 const repasswd = document.querySelector('#repasswd');
 
 let modal = null;
-try {
-    modal = new bootstrap.Modal(zipmodal, {});
-} catch (e) {}
 
-joinbtn?.addEventListener('click', () => {
+joinbtn?.addEventListener('click', ()=>{
     if (joinfrm.userid.value == '') alert('아이디를 입력하세요!!');
     else if (joinfrm.passwd.value == '') alert('비밀번호를 입력하세요!!');
     else if (joinfrm.repasswd.value == '') alert('비밀번호 확인을 입력하세요!!');
     else if (joinfrm.repasswd.value != joinfrm.passwd.value) alert('비밀번호가 서로 일치하지 않아요!!');
     else if (joinfrm.zip1.value == '' || joinfrm.zip2.value == '') alert('우편번호를 확인하세요!!');
-    else if (joinfrm.addr1.value == '' || joinfrm.addr2.value == '') alert('나머지 주소를 입력하세요!!');
-    else if (joinfrm.email1.value == '' || joinfrm.email2.value == '' ) alert('이메일을 확인하세요!!');
-    else if (joinfrm.tel1.value == '' || joinfrm.tel2.value == '' ) alert('전화번호를 확인하세요!!');
-    else if (grecaptcha.getResponse() === '') alert('자동가입방지 처리에 동의하세요!!');
-    else if (joinfrm.ckeckuid.value === 'no') alert('아이디 중복 체크하세요!!');
+    else if (joinfrm.addr1.value == '' || joinfrm.addr2.value == '') alert('주소를 확인하세요!!');
+    else if (joinfrm.email1.value == '' || joinfrm.email2.value == '') alert('이메일을 확인하세요!!');
+    else if (joinfrm.tel2.value == '' || joinfrm.tel3.value == '') alert('전화번호를 확인하세요!!');
+    else if (grecaptcha.getResponse() === '') alert('자동가입방지를 확인하세요!!');
+    else if (joinfrm.checkuid.value === 'no') alert('아이디 중복 체크하세요!!');
     else {
         joinfrm.zipcode.value = joinfrm.zip1.value + '-' + joinfrm.zip2.value;
         joinfrm.email.value = joinfrm.email1.value + '@' + joinfrm.email2.value;
@@ -82,6 +71,7 @@ joinbtn?.addEventListener('click', () => {
         joinfrm.action = '/join/joinok';
         joinfrm.submit();
     }
+
 });
 
 zpmdbtn?.addEventListener('click', () => {
@@ -90,41 +80,44 @@ zpmdbtn?.addEventListener('click', () => {
     }
     dong.value = '';
 
-    modal.show();
+    let mymodal = null;
+    try {
+        mymodal = new bootstrap.Modal(zipmodal, {});
+        modal = mymodal;
+    } catch (e) { }
+
+    mymodal.show();
 });
 
 const showzipaddr = (jsons) => {
-    // for(idx in jsons) {
-    //     console.log(jsons[idx] );
-    // }
     jsons = JSON.parse(jsons);
     let addrs = '';
     jsons.forEach(function (data, idx) {
         let bunji = (data['bunji'] !== null) ? data['bunji'] : '';
-        addrs += `<option>${data['zipcode']} ${data['sido']}  ${data['gugun']}  ${data['dong']}  ${bunji}</option>`;
+        addrs += `<option>${data['zipcode']} ${data['sido']} 
+             ${data['gugun']} ${data['dong']} ${bunji}</option>`;
     });
-    while (addrlist.lastChild) {
+    while(addrlist.lastChild) {
         addrlist.removeChild(addrlist.lastChild);
     }
     addrlist.innerHTML = addrs;
 };
 
-zipbtn?.addEventListener('click',() => {
+zipbtn?.addEventListener('click', ()=> {
     if (dong.value === '') {
-        alert('검색할 동이름을 입력하세요!!')
+        alert('검색할 동이름을 입력하세요!!');
         return;
     }
     const url = '/join/zipcode?dong=' + dong.value;
     fetch(url).then(response => response.text())
         .then(text => showzipaddr(text));
-
-})
+});
 
 sendzip?.addEventListener('click', () => {
     let addr = addrlist.value;
     if (addr !== '') {
         // 123-456 서울 구로구 구로1동
-        let zip = addr.split(' ')[0]; // 123-456
+        let zip = addr.split(' ')[0];  // 123-456
         joinfrm.zip1.value = zip.split('-')[0];
         joinfrm.zip2.value = zip.split('-')[1];
 
@@ -135,22 +128,21 @@ sendzip?.addEventListener('click', () => {
     } else {
         alert('주소를 선택하세요!!');
     }
-})
+});
 
 email3?.addEventListener('change', () => {
-    if(email3.value === '직접입력하기') {
+    if (email3.value === '직접입력하기') {
         joinfrm.email2.readOnly = false;
         joinfrm.email2.value = '';
-    } else if(email3.value !== '선택하세요') {
+    } else if (email3.value !== '선택하세요') {
         joinfrm.email2.readOnly = true;
         joinfrm.email2.value = email3.value;
-
     }
-})
+});
 
 dong?.addEventListener('keydown', (e) => {
-    if (e.keyCode === 13) { // 엔터키를 누르면
-        e.preventDefault(); // 이벤트 전파방지
+    if (e.keyCode === 13) {  // 엔터키를 누르면
+        e.preventDefault();  // 이벤트 전파방지
     }
 })
 
@@ -166,6 +158,7 @@ const styleCheckuid = (chkuid) => {
     }
     uidmsg.innerText = msg;
 };
+
 userid?.addEventListener('blur', () => {
     if (userid.value === '') {
         uidmsg.innerText = '6~16 자의 영문 소문자, 숫자와 특수기호(_)만 사용할 수 있습니다';
@@ -182,7 +175,6 @@ repasswd?.addEventListener('blur', () => {
     let pmsg = '비밀번호가 서로 일치하지 않습니다!!';
     pwdmsg.style.color = 'red';
 
-
     if ( repasswd.value === joinfrm.passwd.value ) {
         pmsg = '비밀번호는 서로 일치합니다!!';
         pwdmsg.style.color = 'blue';
@@ -190,9 +182,8 @@ repasswd?.addEventListener('blur', () => {
     pwdmsg.innerText = pmsg;
 });
 
-
 // ------------------------------- joinok
-const  go2index = document.querySelector('#go2index');
+const go2index = document.querySelector('#go2index');
 
 go2index?.addEventListener('click', () => {
     location.href = '/';
